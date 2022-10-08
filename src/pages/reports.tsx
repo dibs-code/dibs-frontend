e951bfea2297724cc0c92a9aafd7e2ed6032a213
Ali Beigi <beigi.work@gmail.com>
2022-10-09 02:00:00 +0400

    feat: add coming soon in reports

---
+++ src/pages/reports.tsx
@@ -0,0 +1,15 @@
+import Sidenav from 'components/navigation/sidenav';
+import React from 'react';
+
+const Report = () => {
+  return (
+    <div className={'px-40 py-14'}>
+      <Sidenav></Sidenav>
+      <main className={'pl-84 flex justify-center items-center'}>
+        <h1 className={'inline-block mt-40 bg-white font-light shadow-lottery-inner-card px-12 py-9 rounded-2xl'}>Coming Soon ...</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Report; /* Rectangle 18 */
